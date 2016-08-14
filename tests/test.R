2a65a8b029f7670e9da74077dcbed6bdf74e1a11
J27 <jean.d.marchal@gmail.com>
2016-08-14 15:14:12 +0200

    Misc

--- tests/test.R
+++ tests/test.R
@@ -1,33 +1,13 @@
 library(SpaDES)
 
 ## 1 var
-  # mySim <- simInit(
-  #   times = list(start = 1, end = 1, timeunit = "year"),
-  #   modules = list("fireSense_SizeFit"),
-  #   paths = list(modulePath = " # replace with empty string instead"),
-  #   params = list(fireSense_SizeFit = list(
-  #     formula = list(beta = formula(SUP_HA ~ MDC_JUN),
-  #                    theta = formula(SUP_HA ~ MDC_MJ)),
-  #     a = 1,
-  #     trace = 5,
-  #     data = "dataFireSense_SizeFit"
-  #   )),
-  #   inputs = data.frame(
-  #     files = "Z:/dataFireSense_SizeFit.RData",
-  #     objectName = "dataFireSense_SizeFit",
-  #     functions = "load",
-  #     package = "base",
-  #     stringsAsFactors = FALSE)
-  # )
-
-## 6 var (+CN)
   mySim <- simInit(
     times = list(start = 1, end = 1, timeunit = "year"),
     modules = list("fireSense_SizeFit"),
     paths = list(modulePath = " # replace with empty string instead"),
     params = list(fireSense_SizeFit = list(
-      formula = list(beta = formula(SUP_HA ~ MDC_JUN + HW + DIST + O + WATER),
-                     theta = formula(SUP_HA ~ MDC_MJ + HW + DIST + O + WATER)),
+      formula = list(beta = formula(SUP_HA ~ MDC_JUN),
+                     theta = formula(SUP_HA ~ MDC_MJ)),
       a = 1,
       trace = 5,
       data = "dataFireSense_SizeFit"
@@ -39,5 +19,25 @@ library(SpaDES)
       package = "base",
       stringsAsFactors = FALSE)
   )
+
+## 6 var (+CN)
+  # mySim <- simInit(
+  #   times = list(start = 1, end = 1, timeunit = "year"),
+  #   modules = list("fireSense_SizeFit"),
+  #   paths = list(modulePath = " # replace with empty string instead"),
+  #   params = list(fireSense_SizeFit = list(
+  #     formula = list(beta = formula(SUP_HA ~ MDC_JUN + HW + DIST + O + WATER),
+  #                    theta = formula(SUP_HA ~ MDC_MJ + HW + DIST + O + WATER)),
+  #     a = 1,
+  #     trace = 5,
+  #     data = "dataFireSense_SizeFit"
+  #   )),
+  #   inputs = data.frame(
+  #     files = "Z:/dataFireSense_SizeFit.RData",
+  #     objectName = "dataFireSense_SizeFit",
+  #     functions = "load",
+  #     package = "base",
+  #     stringsAsFactors = FALSE)
+  # )
 
 spades(mySim)
